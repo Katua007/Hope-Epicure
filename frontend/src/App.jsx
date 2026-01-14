001ff6ffa01ff2153ec1de15a09a9a6e5240e5da
katua <katuacyril@gmail.com>
2026-01-14 21:31:31 +0300

    feat: connect Home storefront to root route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/" element={<h1 className="p-8 text-center">Customer Storefront Coming Soon...</h1>} />
       </Routes>
     </Router>
   );
